dae854b1c0d8faac4f873f350d629ccd618cb51f
Erin Kado-Fong <kadofong@princeton.edu>
2020-11-17 09:40:18 -0500

    add setup.py

---
+++ mockphot/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup (
+    name = 'merian_mockphot',
+    version='0.0.1',
+    author='Erin Kado-Fong',
+    description='Generate mock photometry for Merian I/O tests',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7'
+    )
